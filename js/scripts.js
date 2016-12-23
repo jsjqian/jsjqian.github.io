89cf32de0815fe138bec44d4eb2ff3080e809e9b
jsjqian <jack.qian97@gmail.com>
2016-12-23 07:57:08 -0500

    Add new styles, resume, and scripts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(function () {
+    var $els = $('div[id^=titles]'),
+        i = 0,
+        len = $els.length;
+
+    $els.slice(1).hide();
+    setInterval(function () {
+        $els.eq(i).fadeOut(function () {
+            i = (i + 1) % len
+            $els.eq(i).fadeIn(1000);
+        })
+    }, 6000)
+})
