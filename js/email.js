0542bc1c3c79433018a6035c9a7daa501019c16e
jsjqian <jack.qian97@gmail.com>
2017-01-12 15:50:06 -0500

    Clean up files and add fades

--- js/email.js
+++
@@ -1,7 +0,0 @@
-function sendEmail() {
-  var email = document.getElementById('email').value;
-  var name = document.getElementById('name').value;
-  var message = document.getElementById('message').value;
-  window.location = 'mailto:jack.qian97@gmail.com?subject='+name+
-  ' '+email+'&body='+message;
-}
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-jQuery(function () {
-    var $els = $('div[id^=titles]'),
-        i = 0,
-        len = $els.length;
+$(function(){  
+  $('#titles1').hide().fadeIn(1000);
 
-    $els.slice(1).hide();
-    setInterval(function () {
-        $els.eq(i).fadeOut(function () {
-            i = (i + 1) % len
-            $els.eq(i).fadeIn(1000);
-        })
-    }, 4000)
+  $('.fa').hide().fadeIn(2500).animate({
+    'top': '10%'
+    }, {duration: 2000, queue: false}, function() {
+    });
+});
+
+$(function () {
+  var $els = $('div[id^=titles]'),
+    i = 0,
+    len = $els.length;
+
+  $els.slice(1).hide();
+  setInterval(function () {
+    $els.eq(i).fadeOut(function () {
+      i = (i + 1) % len
+      $els.eq(i).fadeIn(1000);
+    })
+  }, 3000)
 })
