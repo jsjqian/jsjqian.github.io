a45e6967db4f6ca0b663081bb8fbdf8ba9cf4ebe
Jack Qian <jack.qian97@gmail.com>
2022-07-21 01:07:26 -0700

    Use styled components
    Create menu
    Add new fonts
    Remove unused styles

---
+++ components/Menu.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Menu = () => {
+  return (
+    <Container>
+      <Navbar>
+        <Button href='/past'>past lives</Button>
+        <Button href='/blog'>thoughts</Button>
+        <Button href='/contact'>correspondence</Button>
+      </Navbar>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+	position: sticky;
+  top: 0;
+  width: 100%;
+`
+
+const Navbar = styled.div`
+  display: flex;
+  justify-content: right;
+  padding: 2rem;
+`
+
+const Button = styled.a`
+  color: #fabd2f;
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+`
+
+export default Menu
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,57 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Menu from '../components/Menu'
+import styled from "styled-components";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <Container>
+      <Menu />
       <Head>
-        <title>Jack Qian (@jsjqian)</title>
+        <title>jack qian (@jsjqian)</title>
         <meta name="description" content="@jsjqian" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Jack Qian (@jsjqian)
-        </h1>
-      </main>
-    </div>
+      <Main>
+        <Title>
+          jack qian (@jsjqian)
+        </Title>
+        <Description>
+          yet another developer
+        </Description>
+      </Main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  background: #1d2021;
+  padding: 0 2rem;
+`
+
+const Main = styled.div`
+  min-height: 100vh;
+  color: #ebdbb2;
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+`
+
+const Description = styled.h1`
+  margin: 4rem 0;
+  line-height: 1.5;
+  font-size: 1.5rem;
+  text-align: center;
+`
 
 export default Home
